fc5579077da078d2909a61307cf25bee87dc4c20
RJame <RJame@DESKTOP-TF28NVE>
2016-07-27 00:07:07 -0400

    Add project files.

---
+++ COMP2007-S2016-Lesson10CC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_S2016_Lesson10CC.Startup))]
+namespace COMP2007_S2016_Lesson10CC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
